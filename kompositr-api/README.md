3252420270e603d1ffd99b1781cdc856411f36a0
Matt <mwharri@gmail.com>
2017-02-25 19:59:37 -0600

    Initial commit.
    Hapi plumbing
    Build step documentation updated
    Non functioning Dockerfile

---
+++ kompositr-api/README.md
@@ -0,0 +1,7 @@
+## How to run
+
+    npm install
+    node app.js
+    
+## How to build and deploy on GCE
+Coming soon..
---
+++ kompositr-api/app.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({ port: 3000, host: 'localhost' });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
+server.start((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+});
+
+server.register(require('inert'), (err) => {
+
+    if (err) {
+        throw err;
+    }
+
+    server.route({
+        method: 'GET',
+        path: '/hello',
+        handler: function (request, reply) {
+            reply.file('./public/hello.html');
+        }
+    });
+});
+
