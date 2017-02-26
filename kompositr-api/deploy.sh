b3f75191e60fdb767e880baf49c31551cb5a6b60
Matt <mwharri@gmail.com>
2017-02-26 14:20:57 -0600

    Fix deployment script permission issues

--- kompositr-api/deploy.sh
+++ kompositr-api/deploy.sh
@@ -5,4 +5,4 @@ set -e
 
 sudo /opt/google-cloud-sdk/bin/gcloud docker push us.gcr.io/${PROJECT_NAME}/kompositr-api
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl patch deployment kompositr-api -p '{"spec":{"template":{"spec":{"containers":[{"name":"kompositr-api","image":"us.gcr.io/kompositr/kompositr-api:'"$CIRCLE_SHA1"'"}]}}}}'
+sudo kubectl patch deployment kompositr-api -p '{"spec":{"template":{"spec":{"containers":[{"name":"kompositr-api","image":"us.gcr.io/kompositr/kompositr-api:'"$CIRCLE_SHA1"'"}]}}}}'
