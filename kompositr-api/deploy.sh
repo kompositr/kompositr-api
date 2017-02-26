81ffc8337e0a0b6fb8add5f73c53de2b1fe1b91b
Matt <mwharri@gmail.com>
2017-02-26 14:30:00 -0600

    Fix issue with kubectl not having proper auth privileges

--- kompositr-api/deploy.sh
+++ kompositr-api/deploy.sh
@@ -4,5 +4,6 @@
 set -e
 
 sudo /opt/google-cloud-sdk/bin/gcloud docker push us.gcr.io/${PROJECT_NAME}/kompositr-api
+export GOOGLE_APPLICATION_CREDENTIALS="${HOME}/account-auth.json"
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 sudo kubectl patch deployment kompositr-api -p '{"spec":{"template":{"spec":{"containers":[{"name":"kompositr-api","image":"us.gcr.io/kompositr/kompositr-api:'"$CIRCLE_SHA1"'"}]}}}}'
