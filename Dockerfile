eeb1127a397e7e844572bea86538e8082100e272
Matt <mwharri@gmail.com>
2017-03-04 13:39:22 -0600

    fix issues with docker build

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 FROM node:boron
 RUN mkdir -p /kompositr/src
-WORKDIR /kompositr/src
+WORKDIR /kompositr
 COPY package.json /kompositr
 RUN npm install
-COPY config /kompositr/src/config
+COPY config /kompositr/config
 COPY src/server /kompositr/src/server
 COPY src/app /kompositr/src/app
 EXPOSE 8080
-CMD [ "node", "/kompositr/src/server/index.js" ]
+CMD [ "node", "src/server/index.js" ]
--- scripts/run-container.sh
+++ scripts/run-container.sh
@@ -3,7 +3,7 @@
 # Exit on any error
 set -e
 
-docker build -t kompositr/kompositr-api:local .
-docker tag kompositr/kompositr-api:local kompositr/kompositr-api:local
-docker run -d -p 8080:8080 kompositr/kompositr-api:latest
-curl http://localhost:8080/kompositions/
+docker build --no-cache -t kompositr/kompositr-api:local2 .
+docker tag kompositr/kompositr-api:local2 kompositr/kompositr-api:latest
+docker run -d -p 8080:8080 kompositr/kompositr-api:latest; sleep 5
+curl http://localhost:8080/
